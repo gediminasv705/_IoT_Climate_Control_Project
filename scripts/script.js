d066eedd188ec88072c0ae9b32de2c6fa3debe75
gediminasv705 <vebra.gediminas@gmail.com>
2020-01-03 23:13:53 +0200

    Navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,44 @@
 
+////Čia prikabinu meniu, kai nuvažiuoja į viršų
 
+$('document').ready(function () {
+
+    var navbar = document.getElementById("navbar");
+    navbarPosition = navbar.offsetTop;
+
+    console.log(navbarPosition);
+
+
+    window.addEventListener("scroll", function (e) {
+
+        var scroll = this.scrollY;
+
+        console.log(scroll);
+
+
+        if (scroll > navbarPosition) {
+
+            navbar.classList.add("meniu-fixed");
+            console.log("fixed");
+
+        } else {
+
+            navbar.classList.remove("meniu-fixed");
+            console.log("not fixed");
+
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+});
 
 
