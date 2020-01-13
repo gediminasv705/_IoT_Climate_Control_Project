042a06961419dba0642c5c6f269ee9a8b86db205
gediminasv705 <vebra.gediminas@gmail.com>
2020-01-13 19:40:57 +0200

    sensibo GET completed

--- scripts/sensibo_info.php
+++ scripts/sensibo_info.php
@@ -16,7 +16,7 @@ $curl = curl_init($request_url);
 
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_HTTPHEADER, [
-  'Content-Type: application/json',
+  'Content-Type: application/json'
 ]);
 $response = curl_exec($curl);
 curl_close($curl);
@@ -68,22 +68,21 @@ $collection_name = '/api/v2/pods/' . $device_id . '/acStates/on?apiKey=' . $api_
 
 $host = 'https://home.sensibo.com';
 
-$request_url = $host . '/' . $collection_name;
+$request_url = $host . $collection_name;
 $curl = curl_init($request_url);
 
-$data = [
-  'newValue' => 'true'
-];
+$data = '{
+  "newValue": false
+}';
 
 print_r($data);
 
-curl_setopt($curl, CURLOPT_URL, $host);
+curl_setopt($curl, CURLOPT_URL, $request_url);
 curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PATCH');
 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_HTTPHEADER, [
-  'Content-Type: application/x-www-form-urlencoded',
-  'Referer: localhost/_IoT_Climate_Control_Project/sensibo_html.php'
+  'Content-Type: application/json'
 ]);
 
 $response = curl_exec($curl);
