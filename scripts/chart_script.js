f0df31db311c69003580b7cff65a4ff180710cde
gediminasv705 <vebra.gediminas@gmail.com>
2020-01-20 13:51:48 +0200

    pakeiciau chart

--- scripts/chart_script.js
+++ scripts/chart_script.js
@@ -4,10 +4,10 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['1', '5', '6', '8', '11', '20'],
+        labels: ['1', '2', '3', '4', '5', '6', '7', '11', '20', '8', '11', '20', '20', '8', '11', '20', '20', '8', '11', '20'],
         datasets: [{
             label: 'Temperature changes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [22, 23, 24, 28, 2, 3, 12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
